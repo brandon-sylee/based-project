1f221651460710faa67d5f16c26fcceb1e5c56de
brandonLee <dd.stay.hf@gmail.com>
2016-07-28 14:40:00 +0900

    게시판 만드는 중  - 날짜 UTC 기준 및 지역화 시간 추가

--- src/main/java/com/brandon/services/boards/models/NormalBoardModel.java
+++ src/main/java/com/brandon/services/boards/models/NormalBoardModel.java
@@ -1,16 +1,18 @@
 package com.brandon.services.boards.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.validation.constraints.Future;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 /**
  * Created by brandon Lee(lz90011@linecorp.com) on 2016-07-04.
@@ -28,5 +30,27 @@ public class NormalBoardModel implements Serializable, BoardAttributes {
     @NotBlank(message = "{validation.blank}")
     private String content;
     private String creator;
+
+    @JsonIgnore
     private LocalDateTime created;
+
+    @JsonProperty("created")
+    public long getCreatedOnUTCBased() {
+        return created.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+
+    @JsonIgnore
+    public String getLocalizedDateTimeLong() {
+        return created.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG));
+    }
+
+    @JsonProperty("localizedDateTime")
+    public String getLocalizedDateTime() {
+        return created.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+    }
+
+    @JsonIgnore
+    public String getLocalizedDateTimeShort() {
+        return created.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+    }
 }
