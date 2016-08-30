db2938293830cf2577d7e1bff9c877b5357e8332
brandonLee <dd.stay.hf@gmail.com>
2016-08-30 17:02:27 +0900

    integration websocket 추가   - nginx proxy 설정 이슈 해결해야함

---
+++ src/main/java/com/brandon/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.brandon;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BasedProjectApplication.class);
+	}
+
+}
