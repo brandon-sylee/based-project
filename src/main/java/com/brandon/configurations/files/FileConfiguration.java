b2b8a915a33e4365ac57556abc5cf58cca31775b
brandonLee <dd.stay.hf@gmail.com>
2016-08-31 16:26:17 +0900

    File 관련 설정 추가중

--- src/main/java/com/brandon/configurations/files/FileConfiguration.java
+++ src/main/java/com/brandon/configurations/files/FileConfiguration.java
@@ -1,27 +1,43 @@
 package com.brandon.configurations.files;
 
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.dsl.core.Pollers;
-import org.springframework.integration.dsl.support.Transformers;
+import org.springframework.messaging.MessageChannel;
 
-import java.io.File;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
+ * @link https://github.com/spring-projects/spring-integration-samples/blob/master/intermediate/multipart-http/src/main/resources/META-INF/spring/integration/http-outbound-config.xml
  * Created by brandon Lee on 2016-08-31.
  */
 @Configuration
 public class FileConfiguration {
+    private final Logger logger = getLogger(getClass());
     private final String INBOUND_PATH = "d:/";
 
-    /*@Bean
-    public IntegrationFlow fileReadingFlow() {
-        return IntegrationFlows
-                .from(s -> s.file(new File(INBOUND_PATH)).patternFilter("*.txt"), e -> e.poller(Pollers.fixedDelay(1000)))
-                .transform(Transformers.fileToString())
-                .channel("processFileChannel")
+    /**
+     * 파일 다운로드 Flow
+     * @return
+     */
+    @Bean
+    public IntegrationFlow downloadFlow() {
+        IntegrationFlows
+                .from("downloadChannel")
                 .get();
-    }*/
+        return null;
+    }
+
+    /**
+     * 파일 업로드 Flow
+     * @return
+     */
+    @Bean
+    public IntegrationFlow uploadFlow() {
+        return null;
+    }
 }
